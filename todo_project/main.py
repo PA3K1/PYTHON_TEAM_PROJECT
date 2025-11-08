af0d793a432bc57fef06c704345db04f2c1e7f3b
PA3K1 <evstropovmatvej936@gmail.com>
2025-11-08 20:13:42 +0300

    пваоыиVESTHY

--- todo_project/main.py
+++ todo_project/main.py
@@ -9,34 +9,31 @@ deleted_tasks = []
 
 @app.route('/')
 def index():
+    edit_id = request.args.get('edit', -1, type=int)
     return render_template('index.html',
                          active_tasks=active_tasks,
                          completed_tasks=completed_tasks,
-                         deleted_tasks=deleted_tasks)
+                         deleted_tasks=deleted_tasks,
+     edit_id=edit_id)
 
 @app.route('/add', methods=['POST'])
 def add_task():
     task = request.form['task']
     active_tasks.append(task)
-    print(f"Добавлена задача: {task}")
     return index()
 
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
     if 0 <= task_id < len(active_tasks):
-        # Перемещаем задачу из активных в удаленные
         deleted_task = active_tasks.pop(task_id)
         deleted_tasks.append(deleted_task)
-        print(f"Задача удалена: {deleted_task}")
     return index()
 
 @app.route('/complete/<int:task_id>')
 def complete_task(task_id):
     if 0 <= task_id < len(active_tasks):
-        # Перемещаем задачу из активных в выполненные
         completed_task = active_tasks.pop(task_id)
         completed_tasks.append(completed_task)
-        print(f"Задача выполнена: {completed_task}")
     return index()
 
 @app.route('/edit/<int:task_id>', methods=['POST'])
@@ -44,7 +41,6 @@ def edit_task(task_id):
     if 0 <= task_id < len(active_tasks):
         new_text = request.form['edited_task']
         active_tasks[task_id] = new_text
-        print(f"Задача изменена: {new_text}")
     return index()
 
 if __name__ == '__main__':
